d19a1d2afc99c6b8baf70e14356cd48a64cf0deb
sanjana bharati <itz.sanjana555@gmail.com>
2026-06-06 00:48:45 +0530

    Solved Right Rotate Array By K Places

---
+++ Arrays/RightRotateArrayByK.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        int n = nums.length;
+        k = k % n;
+
+        int[] temp = new int[k];
+
+        // Store last k elements
+        for (int i = 0; i < k; i++) {
+            temp[i] = nums[n - k + i];
+        }
+
+        // Shift remaining elements right
+        for (int i = n - k - 1; i >= 0; i--) {
+            nums[i + k] = nums[i];
+        }
+
+        // Place stored elements at the beginning
+        for (int i = 0; i < k; i++) {
+            nums[i] = temp[i];
+        }
+    }
+}
